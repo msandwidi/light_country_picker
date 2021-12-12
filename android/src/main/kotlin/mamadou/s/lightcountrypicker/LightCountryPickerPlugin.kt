0a0b257f6f534e62d0750057d92bb77f23c61de0
Mamadou <shoudou@outlook.com>
2021-12-12 08:15:12 -0500

    upgraded to android embeded v2

--- android/src/main/kotlin/mamadou/s/lightcountrypicker/LightCountryPickerPlugin.kt
+++ android/src/main/kotlin/mamadou/s/lightcountrypicker/LightCountryPickerPlugin.kt
@@ -1,5 +1,8 @@
 package mamadou.s.lightcountrypicker
 
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
@@ -8,7 +11,7 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.util.*
 import kotlin.collections.HashMap
 
-class LightCountryPickerPlugin(): MethodCallHandler {
+class LightCountryPickerPlugin(): FlutterPlugin, MethodCallHandler {
 
   companion object {
     private val CHANNEL = "mamadou.s/light_country_picker"
@@ -21,13 +24,38 @@ class LightCountryPickerPlugin(): MethodCallHandler {
     }
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result): Unit {
+    /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "mamadou.s/light_country_picker")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result): Unit{
+    if (call.method.equals(GET_COUNTRY_NAMES)) {
+      result.success(getAllCountryNames(call.argument("isoCodes")))
+    }else if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
+  /*override fun onMethodCall(call: MethodCall, result: Result): Unit {
     if (call.method.equals(GET_COUNTRY_NAMES)) {
       result.success(getAllCountryNames(call.argument("isoCodes")))
     } else {
       result.notImplemented()
     }
-  }
+  }*/
 
   private fun getAllCountryNames(isoCodes: ArrayList<String>?): HashMap<String, String> {
     val localCountries = HashMap<String, String>()
